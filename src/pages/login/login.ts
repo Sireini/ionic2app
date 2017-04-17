fca2cf955878c6c44819d3a79481e3703529df7d
smgjreinieren <smgj.reinieren@gmail.com>
2017-04-18 01:03:32 +0200

    Fixed the login screen with redirect and loading screen

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,20 +1,55 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
 import * as $ from 'jquery';
+
+import { TabsPage } from '../tabs/tabs';
 
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
 export class LoginPage {
+  // tokenValue: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public loading: LoadingController) {
     
   }
 
-  changeColor(){
-    $('#x').text('white');
-    console.log('test');
+  getSchema(tokenValue){
+    setTimeout(() => {
+    this.navCtrl.push(TabsPage);
+
+    $.ajax({
+        type: "POST",
+        url:  "http://sylvanreinieren.com/fysioWebapp/php/get_schema.php",
+        data: { dataString: tokenValue },
+        cache: false,
+        success: function(data){
+            $('#myDiv').html(data);
+        }
+      });
+    }, 2000);
   }
+
+  ionViewLoaded(tokenValue) {
+    // Create the popup
+    let loadingPopup = this.loading.create({
+      content: 'Laden persoonlijk schema...'
+    });
+
+    // Show the popup
+    loadingPopup.present();
+
+    setTimeout(() => {
+      loadingPopup.dismiss();
+    }, 2000);
+
+    this.getSchema(tokenValue);
+  }
+
+  // changeColor(){
+  //   $('#x').text('white');
+  //   console.log('test');
+  // }
 
 }
